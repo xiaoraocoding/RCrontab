aebf2d2cb39829d201f1243a3cb6d43263d72e5b
xiaoraocoding <3490755245@qq.com>
2022-04-20 19:12:02 +0800

    删除指定的mogodb

---
+++ demo11/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
+
+type TimeBeforeCond struct {
+
+	Before int64 `bson:"$lt"`
+}
+
+type DeleteCond struct {
+	beforeCond TimeBeforeCond `bson:"timePoint.startTime"`
+
+}
+func main() {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://47.96.162.129:27017"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+
+	data := client.Database("cron")
+
+	colletion := data.Collection("log")
+
+	delCond := &DeleteCond{
+		beforeCond: TimeBeforeCond{Before: time.Now().Unix()},
+	}
+
+	colletion.DeleteMany(context.TODO(),delCond)
+
+
+
+
+
+}
