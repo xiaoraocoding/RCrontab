0bb4ece94ea9a032679c97d003f7d965e8cf8b45
xiaoraocoding <3490755245@qq.com>
2022-04-23 19:33:32 +0800

    实现服务发现

---
+++ DEMO/demo12/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"RCrontab/common"
+	"fmt"
+	"net"
+)
+
+func main() {
+	ipv4,err:= getLocalIP()
+	if err != nil {
+		fmt.Println("err")
+	}
+	fmt.Println(ipv4)
+
+}
+
+
+// 获取本机网卡IP
+func getLocalIP() (ipv4 string, err error) {
+	var (
+		addrs []net.Addr
+		addr net.Addr
+		ipNet *net.IPNet // IP地址
+		isIpNet bool
+	)
+	// 获取所有网卡
+	if addrs, err = net.InterfaceAddrs(); err != nil {
+		return
+	}
+	// 取第一个非lo的网卡IP
+	for _, addr = range addrs {
+		// 这个网络地址是IP地址: ipv4, ipv6
+		if ipNet, isIpNet = addr.(*net.IPNet); isIpNet && !ipNet.IP.IsLoopback() {
+			// 跳过IPV6
+			if ipNet.IP.To4() != nil {
+				ipv4 = ipNet.IP.String()	// 192.168.1.1
+				return
+			}
+		}
+	}
+
+	err = common.ERR_NO_LOCAL_IP_FOUND
+	return
+}
--- worker/main/main.go
+++ worker/main/main.go
@@ -10,7 +10,7 @@ func main() {
 	worker.InitJobMgr()
 
 	worker.InitLogSink()
-
+    worker.InitRegister()
 
 
 
